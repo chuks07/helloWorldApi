8023a93d130fe225389dcaa8605d57c8e9065b7f
chuks umenwa <chuksumenwa@yahoo.com>
2021-08-31 11:12:33 +0100

    live server

--- main.go
+++ main.go
@@ -3,3 +3,20 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 )
+
+func main ( ){
+	router:= gin.Default()
+
+	router.GET("/chuks", helloWorldhandler)
+
+	_ =router.Run(":3000")
+}
+
+func helloWorldhandler(c *gin.Context) {
+	c.JSON(200,gin.H{
+		"message":"hello world",
+		"status": "we are live",
+		"age": "25",
+		"name": "father ruben",
+	})
+}
